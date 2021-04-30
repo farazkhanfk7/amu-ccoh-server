e5907fc5040d4f5ba156949c1abfad93faec6cec
farazkhanfk7 <farazkhan138@gmail.com>
2021-04-30 12:40:07 +0530

    webui

--- covid/settings.py
+++ covid/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'multiselectfield',
-    'api'
+    'api',
+    'webui'
 ]
 
 REST_FRAMEWORK = {
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'covid.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,4 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
--- covid/urls.py
+++ covid/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('webui.urls')),
     path('api/v1/',include('api.urls')),
     path('api/auth/',include('rest_framework.urls'))
 ]
